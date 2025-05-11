993560c65c1b11ab817de36be2596f3240d37562
Eric Neder <ericbiu@hotmail.com>
2025-05-11 19:42:56 -0300

    melhorias com a bibliotecafaker

--- tests/e2e/leads.spec.js
+++ tests/e2e/leads.spec.js
@@ -1,5 +1,5 @@
 // @ts-check
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 const { LandingPage } = require('../pages/LandingPage')
 const { Toast } = require('../pages/Components')
 const { MoviesPage } = require('../pages/MoviesPage')
@@ -16,17 +16,37 @@ test.beforeEach(async ({ page }) => {
 })
 
 test('deve cadastrar um lead na fila de espera', async ({ page }) => {
+  const leadName = faker.person.fullName()
+  const leadEmail = faker.internet.email()
 
   await landingPage.visit()
   await landingPage.openLeadModal()
-  await landingPage.submitLeadForm(faker.person.firstName(), faker.internet.email())
+  await landingPage.submitLeadForm(leadName, leadEmail)
   const message = 'Agradecemos por compartilhar seus dados conosco. Em breve, nossa equipe entrará em contato!'
   await toast.toastHaveText(message)
+});
 
+test('não deve cadastrar quando o email ja existe', async ({ page, request }) => {
+  const leadName = faker.person.fullName()
+  const leadEmail = faker.internet.email()
+
+  const newLead = await request.post('http://localhost:3333/leads',{
+    data:{
+       name: leadName,
+      email: leadEmail
+    }
+  })
+  
+  expect(newLead.ok()).toBeTruthy()
+
+  await landingPage.visit()
+  await landingPage.openLeadModal()
+  await landingPage.submitLeadForm(leadName, leadEmail)
+  const message = 'O endereço de e-mail fornecido já está registrado em nossa fila de espera.'
+  await toast.toastHaveText(message)
 });
 
 test('nao deve cadastrar um email incorreto', async ({ page }) => {
-
   await landingPage.visit()
   await landingPage.openLeadModal()
   await landingPage.submitLeadForm('eric cabral neder', 'erik.ericcabralmail.com')
@@ -34,7 +54,6 @@ test('nao deve cadastrar um email incorreto', async ({ page }) => {
 });
 
 test('nao deve cadastrar quando o nome nao e preenchido', async ({ page }) => {
-
   await landingPage.visit()
   await landingPage.openLeadModal()
   await landingPage.submitLeadForm('', 'erik.ericcabral@mail.com')
@@ -42,16 +61,13 @@ test('nao deve cadastrar quando o nome nao e preenchido', async ({ page }) => {
 });
 
 test('nao deve cadastrar um o email nao e preenchido', async ({ page }) => {
-
   await landingPage.visit()
   await landingPage.openLeadModal()
   await landingPage.submitLeadForm('eric cabral neder', '')
   await landingPage.alertHaveText('Campo obrigatório')
-
 });
 
 test('nao deve cadastrar quando nenhum campo e preenchido', async ({ page }) => {
-
   await landingPage.visit()
   await landingPage.openLeadModal()
   await landingPage.submitLeadForm('', '')
@@ -60,3 +76,4 @@ test('nao deve cadastrar quando nenhum campo e preenchido', async ({ page }) => 
     'Campo obrigatório'
   ])
 });
+
