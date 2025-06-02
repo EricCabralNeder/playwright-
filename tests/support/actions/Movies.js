1dcdb199bf73182f95e909204b314f58bdcdfca5
Eric Neder <ericbiu@hotmail.com>
2025-06-02 11:07:37 -0300

    Conclusao das tarefas

--- tests/support/actions/Movies.js
+++ tests/support/actions/Movies.js
@@ -10,7 +10,7 @@ export class Movies {
         await this.page.locator('a[href$="register"]').click()
     }
 
-    async create(title, overview, company, release_year, cover) {
+    async create(title, overview, company, release_year, cover, featured) {
         await this.goForm()
         await this.page.getByLabel('Titulo do filme').fill(title)
 
@@ -24,13 +24,14 @@ export class Movies {
 
         await this.page.locator('#select_year .react-select__indicator').click()
         await this.page.locator('.react-select__option').filter({ hasText: release_year }).click()
-
         await this.page.locator('input[name=cover]')
             .setInputFiles('tests/support/fixtures' + cover)
-
+       
+        if(featured) {
+            await this.page.locator('.featured .react-switch').click()
+        }
         await this.page.getByRole('button', { name: 'Cadastrar' }).click()
     }
-
 
     async IdentityMovies(título, sinopse, distribuidora, lançamento) {
         await this.goForm()
